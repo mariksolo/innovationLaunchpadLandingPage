7c6ff0a412d317ac64788c25fdef9bed5c145a54
Mark Solomonik <marik.solomonik@gmail.com>
2019-06-08 15:26:45 -0700

    added email form, navbar, and some placeholder images and text. Changed some scss

--- src/index.js
+++ src/index.js
@@ -2,35 +2,117 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../materialize-src/sass/materialize.scss';
-import { Button, Icon, Row, Col, Card } from 'react-materialize';
+import '../materialize-src/js/bin/materialize.js';
+import {
+  Button,
+  Icon,
+  Row,
+  Col,
+  Card,
+  Navbar,
+  NavItem,
+  TextInput,
+  Container
+} from 'react-materialize';
 import IL3 from 'IL3.jpg';
 import ilp from 'ilp.jpg';
 import ilp2 from 'ilp2.jpg';
 import ilp3 from 'ilp3.jpg';
 
-let Home = () => {
-  return (
-    <div>
-      <h1>Hello there World!</h1>
-      <p>Hola</p>
-      <Button waves="light">
-        <Icon>thumb_up</Icon>
-      </Button>
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <Row>
-        <Col m={6} s={12}>
-          <Card
-            header={<img src={ilp} />}
-            className="blue-grey darken-1"
-            textClassName="white-text"
-            title="Card title"
-          >
-            I am a very simple card.
-          </Card>
-        </Col>
-      </Row>
-    </div>
-  );
-};
+    this.state = {
+      email: '',
+      name: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
+
+  handleSubmit = event => {};
+  render() {
+    return (
+      <div>
+        <Navbar brand={<a />} alignLinks="right">
+          <NavItem href="">Getting started</NavItem>
+          <NavItem href="components.html">Components</NavItem>
+        </Navbar>
+
+        <br />
+        <Row>
+          <Col l={5} offset="l1" className="homeImage">
+            <img src={ilp} />
+          </Col>
+
+          <Col l={4} offset="l1" className="homeForm">
+            <br />
+            <br />
+            <Row>
+              <h2 className="mainText">Try the world's #1 CRM built for small business growth.</h2>
+            </Row>
+            <Row>
+              <h5 className="secondaryText">
+                Get Salesforce essentials, our all-in-one sales and service solution for just $25 a
+                month.
+              </h5>
+            </Row>
+            <Row>
+              <TextInput label="Full Name" id="name" onChange={this.handleChange} />
+
+              <TextInput
+                email
+                label="Email"
+                id="email"
+                error="Invalid Email"
+                onChange={this.handleChange}
+              />
+            </Row>
+            <Row>
+              <Button waves="light" onClick={this.handleSubmit}>
+                button
+              </Button>
+            </Row>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col l={2}>
+            <Icon medium>cloud</Icon>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et efficitur
+              velit. Proin mattis neque a enim sollicitudin, id tincidunt eros faucibus. Curabitur
+              vitae tellus sollicitudin, venenatis massa sed, blandit mauris. Cras lacinia id ante
+              ac ultrices. Sed ac ex iaculis, convallis turpis scelerisque, ullamcorper justo.
+            </p>
+          </Col>
+
+          <Col l={2}>
+            <Icon medium>cloud</Icon>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et efficitur
+              velit. Proin mattis neque a enim sollicitudin, id tincidunt eros faucibus. Curabitur
+              vitae tellus sollicitudin, venenatis massa sed, blandit mauris. Cras lacinia id ante
+              ac ultrices. Sed ac ex iaculis, convallis turpis scelerisque, ullamcorper justo.
+            </p>
+          </Col>
+
+          <Col l={2}>
+            <Icon medium>cloud</Icon>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et efficitur
+              velit. Proin mattis neque a enim sollicitudin, id tincidunt eros faucibus. Curabitur
+              vitae tellus sollicitudin, venenatis massa sed, blandit mauris. Cras lacinia id ante
+              ac ultrices. Sed ac ex iaculis, convallis turpis scelerisque, ullamcorper justo.
+            </p>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<Home />, document.getElementById('root'));
